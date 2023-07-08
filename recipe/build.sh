093e12687da7ee32a85dd50e942743e249bc6320
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2023-07-08 15:34:41 -0700

    Clean up patch and fix unix build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,6 @@
 
 export CFLAGS="${CFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 
-{{ PYTHON }} -m pip install . --no-deps -vv
+$PYTHON -m pip install . --no-deps -vv
 
 cp extensions/_pywrapfst.h "${PREFIX}/include/_pywrapfst.h"
